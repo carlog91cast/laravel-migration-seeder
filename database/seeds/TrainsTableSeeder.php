d5369d06e7203e1b62c05e1d84f1817a014f20d5
bigeppo <carlog91cast@gmail.com>
2022-09-09 16:49:33 +0200

    add train tables in TrainTableSeeder.php

--- database/seeds/TrainsTableSeeder.php
+++ database/seeds/TrainsTableSeeder.php
@@ -11,6 +11,40 @@ class TrainsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $trains = [
+            [
+                'company' => 'Italo',
+                'departure gate' => 'Macerata',
+                'arrival gate' => 'Torino',
+                'departure time' => '3:00 a.m',
+                'arrival time' => '7:00 a.m',
+                'code' => '325647',
+                'carriage numeber' => '5',
+                'in time' => 'true',
+                'cancelled' => 'false',
+            ],
+            [
+                'company' => 'Trenitalia',
+                'departure gate' => 'Milano',
+                'arrival gate' => 'Roma',
+                'departure time' => '3:00 a.m',
+                'arrival time' => '7:00 a.m',
+                'code' => '4523452',
+                'carriage numeber' => '10',
+                'in time' => 'true',
+                'cancelled' => 'false',
+            ],
+            [
+                'company' => 'PDP Trains',
+                'departure gate' => 'Paperopoli',
+                'arrival gate' => 'Ocopoli',
+                'departure time' => '3:00 a.m',
+                'arrival time' => '7:00 a.m',
+                'code' => '34523452',
+                'carriage numeber' => '14',
+                'in time' => 'false',
+                'cancelled' => 'true',
+            ],
+        ];
     }
 }
